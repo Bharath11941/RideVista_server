8d1cbfe5096f8b66c9f7bc4d958d1b7c484d2ae8
Bharath <bharathpalakkal@gmail.com>
2023-12-15 17:02:45 +0530

    change origin

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ import http from 'http'
 import dbconnect from "./config/mongodb.js"
 dbconnect()
 app.use(cors({
-  // origin:"https://ride-vista-client.vercel.app",
-  origin:"http://localhost:5173",
+  origin:"https://ride-vista-client.vercel.app",
+  // origin:"http://localhost:5173",
   methods:['GET','POST','PUT','PATCH'],
   credentials:true
 }))
